926aa8508b364a6803c03063993a6e98aefb4c52
梁泽祥 <960842214@qq.com>
2019-04-11 21:27:08 +0800

    添加项目文件。

---
+++ 实体生成/Sys.GenerateModel/CongifService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sys.GenerateModel
+{
+    public class CongifService
+    {
+
+
+        /// <summary>
+        /// 初始化ZpMain对象模型数据库
+        /// </summary>
+        /// <param name="path">路径</param>
+        public void InitdbModelForChooseHouse(string path)
+        {
+            var db = DBHelper.DBBase.Blog_MainDBConnection();
+            db.DbFirst.CreateClassFile(path, "Blog_Main.Model");
+        }
+     
+
+    }
+}
